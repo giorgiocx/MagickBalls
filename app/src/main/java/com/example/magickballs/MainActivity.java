d70d4c3418d9c0683698e3d681426930750f3027
giorgiocx <jorge.martineztoro2@mail.udp.cl>
2020-05-09 22:22:07 -0400

    Proyecto MagicBalss: se agregaron as respuestas aleatorias al apretar el boton y es ejecutable.

--- app/src/main/java/com/example/magickballs/MainActivity.java
+++ app/src/main/java/com/example/magickballs/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.magickballs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView foto;
+    private TextView texto;
+    private String[] papaya={"Es cierto", "Definitivamente es así", "Sin duda", "Sí definitivamente", "Puede confiar en él", "Como yo lo veo, sí",
+            "Lo más probable", "Perspectiva buena", "Sí", "Las señales apuntan a sí", "Respuesta confusa intente de nuevo", "Pregunte de nuevo más tarde",
+            "Mejor no decirte ahora", "No puedo predecir ahora", "Concéntrate y pregunta de nuevo", "No cuentes con eso",
+            "Mi respuesta es no", "Mis fuentes dicen que no", "Outlook no es tan bueno", "Muy dudoso"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        foto=findViewById(R.id.ball8);
+        texto=findViewById(R.id.textoSecundario);
+        foto.setOnClickListener(this);
+    }
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.ball8:
+                int numero = new Random().nextInt(papaya.length);
+                texto.setText(papaya[numero]);
+        }
     }
 }
