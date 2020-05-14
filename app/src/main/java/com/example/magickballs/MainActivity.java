5714bc0155ea5bc2acc78e86313d2e3cfaea9591
giorgiocx <jorge.martineztoro2@mail.udp.cl>
2020-05-14 18:01:50 -0400

    cambios de textos

--- app/src/main/java/com/example/magickballs/MainActivity.java
+++ app/src/main/java/com/example/magickballs/MainActivity.java
@@ -13,9 +13,10 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageView foto;
     private TextView texto;
-    private String[] papaya={"Es cierto", "Definitivamente es así", "Sin duda", "Sí definitivamente", "Puede confiar en él", "Como yo lo veo, sí",
-            "Lo más probable", "Perspectiva buena", "Sí", "Las señales apuntan a sí", "Respuesta confusa intente de nuevo", "Pregunte de nuevo más tarde",
-            "Mejor no decirte ahora", "No puedo predecir ahora", "Concéntrate y pregunta de nuevo", "No cuentes con eso",
+    private String[] papaya={"Es cierto", "Definitivamente es así", "Sin duda", "Nunca uses Apple", "Sí definitivamente","Para que quieres saber eso?", "Puede confiar en él", "Como yo lo veo, sí",
+            "Lo más probable", "Perspectiva buena", "Sí", "Las señales apuntan a sí", "Respuesta confusa intente de nuevo", "Pregunte de nuevo más tarde", "No vuelvas a preguntar eso", "shh, los veganos estan escuchando, siempre escuchan",
+            "Dejalo, no te ama", "no", "Ella pregunto lo mismo", "No dejes que tus dudas te carcoman el alma", "Juanes te ama, no lo olvides", " Mi perro pregunta mejores cosas con mas sentido",
+            "Mejor no decirte ahora", "No puedo predecir ahora", "Concéntrate y pregunta de nuevo", "No cuentes con eso","El dolor, es una ilusion de la realidad",
             "Mi respuesta es no", "Mis fuentes dicen que no", "Outlook no es tan bueno", "Muy dudoso"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
